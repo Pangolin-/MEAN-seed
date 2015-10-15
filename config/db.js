c836700bc5bf502eb5e021ef7629421e1b40b493
Manuel Puentes Reed <object.Pangolin@gmail.com>
2015-10-15 12:01:34 -0400

    removed .gitignore folders

--- config/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	url : 'mongodb://pango:shell@localhost/pango'
-}
--- dev/js/controllers/homeCtrl.js
+++
@@ -1,6 +0,0 @@
-angular
-	.module('pangolin')
-	.controller('HomeCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
-		console.log("HomeCtrl");
-		$rootScope.selectedTab = 0;
-	}])
